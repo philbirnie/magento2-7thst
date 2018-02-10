3e096dce0f5400e6ce0b818094ba123befbf7007
Phil Birnie <me@philbirnie.com>
2018-02-09 23:31:08 -0500

    Adds Integration tests db to provisioner

--- vagrant/provision-mysql.sh
+++ vagrant/provision-mysql.sh
@@ -3,6 +3,7 @@
 echo "===================== MYSQL SETUP ====================="
 
 DB="magento2" && export DB
+DB_TEST="${DB}_test" && export DB_TEST
 
 # Drop and Create DB
 echo "DROP DATABASE IF EXISTS ${DB}" | mysql -v
@@ -14,6 +15,20 @@ echo "CREATE USER ${DB} IDENTIFIED BY '${DB}'" | mysql -v
 echo "GRANT ALL PRIVILEGES ON *.* TO ${DB}@'%'" | mysql -v
 echo "FLUSH PRIVILEGES"
 
+# Create Test User
+echo "Creating User for Integration Tests"
+# Drop and Create DB
+echo "DROP DATABASE IF EXISTS ${DB_TEST}" | mysql -v
+echo "CREATE DATABASE ${DB_TEST}" | mysql -v
+
+# Drop and Create User
+echo "DROP USER IF EXISTS ${DB_TEST}" | mysql -v
+echo "CREATE USER ${DB_TEST} IDENTIFIED BY '${DB_TEST}'" | mysql -v
+echo "GRANT ALL PRIVILEGES ON *.* TO ${DB_TEST}@'%'" | mysql -v
+echo "FLUSH PRIVILEGES"
+
+
+
 echo "===================== MYSQL SETUP COMPLETE  CREDENTIALS BELOW ====================="
 
 echo "ROOT PASSWORD: root"
@@ -21,4 +36,9 @@ echo "DB USER: ${DB}"
 echo "DB PASSWORD: ${DB}"
 echo "DB: ${DB}"
 
+echo "INTEGRATION TEST DATABASE USER: ${DB_TEST}"
+echo "INTEGRATION TEST DATABASE PASSWORD: ${DB_TEST}"
+echo "INTEGRATION TEST DATABASE DB: ${DB_TEST}"
+
 exit 0
+
